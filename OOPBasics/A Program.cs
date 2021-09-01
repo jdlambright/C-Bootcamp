7f69f327fcf7e6524973272475d49d5b89ce41b5
jdlambright <64182772+jdlambright@users.noreply.github.com>
2021-09-01 14:51:07 -0400

    fields and properties

---
+++ OOPBasics/A Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OOPBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount myAccount = new BankAccount();
+            myAccount.Balance = 123.34m;
+            Console.WriteLine(myAccount.Balance);
+
+        }
+    }
+}
---
+++ OOPBasics/Fields and Properties/BankAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPBasics
+{
+    class BankAccount
+    {
+        private decimal _balance;
+
+        public decimal Balance
+        {
+            get { return _balance; }
+            set { _balance = value; }
+        }
+
+    }
+}
